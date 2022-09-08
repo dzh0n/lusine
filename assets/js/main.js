a02e2b91199ffb59982945487d2317310f1f9190
Evgeniy Plotnikov <plotnikov.evgeniy@gmail.com>
2022-09-08 13:19:58 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,30 @@ $(document).ready(function () {
             prevEl: '.reviews__prev',
             nextEl: '.reviews__next',
         },
-
+        breakpoints: {
+            576: {
+                slidesPerView: 1.2,
+            },
+            320: {
+                slidesPerView: 1.2,
+            },
+        },
+        on: {
+            afterInit(swiper) {
+                //console.log($('.reviews-slider .swiper-slide-active').attr('aria-label'));
+                setTimeout(function () {
+                    let reviewsSliderCurrent = $('.reviews-slider .swiper-slide-active').attr('aria-label').split(' / ');
+                    $('.reviews-slider .pagination-digits_current').text(reviewsSliderCurrent[0]);
+                    $('.reviews-slider .pagination-digits_total').text(reviewsSliderCurrent[1]);
+                }, 0);
+            }
+        }
     });
+
+    reviewsSlider.on('slideChange', function () {
+        let reviewsSliderCurrent = reviewsSlider.slides.eq(reviewsSlider.activeIndex)[0].ariaLabel.split(' / ');
+        $('.reviews-slider .pagination-digits_current').text(reviewsSliderCurrent[0]);
+        $('.reviews-slider .pagination-digits_total').text(reviewsSliderCurrent[1]);
+    });
+
 });

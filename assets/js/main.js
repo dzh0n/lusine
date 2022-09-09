999b883a20843e265274f856a77d617e63802a4b
Evgeniy Plotnikov <plotnikov.evgeniy@gmail.com>
2022-09-09 12:58:39 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,4 +36,42 @@ $(document).ready(function () {
         $('.reviews-slider .pagination-digits_total').text(reviewsSliderCurrent[1]);
     });
 
+
+    let actionsSlider = new Swiper('.actions-slider', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        navigation: {
+            prevEl: '.reviews__prev',
+            nextEl: '.reviews__next',
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 1,
+
+            },
+            576: {
+                slidesPerView: 1,
+            },
+            320: {
+                slidesPerView: 1,
+            },
+        },
+        on: {
+            afterInit(swiper) {
+                //console.log($('.reviews-slider .swiper-slide-active').attr('aria-label'));
+                setTimeout(function () {
+                    let actionsSliderCurrent = $('.actions-slider .swiper-slide-active').attr('aria-label').split(' / ');
+                    $('.actions-slider .pagination-digits_current').text(actionsSliderCurrent[0]);
+                    $('.actions-slider .pagination-digits_total').text(actionsSliderCurrent[1]);
+                }, 0);
+            }
+        }
+    });
+
+    actionsSlider.on('slideChange', function () {
+        let actionsSliderCurrent = actionsSlider.slides.eq(actionsSlider.activeIndex)[0].ariaLabel.split(' / ');
+        $('.actions-slider .pagination-digits_current').text(actionsSliderCurrent[0]);
+        $('.actions-slider .pagination-digits_total').text(actionsSliderCurrent[1]);
+    });
+
 });

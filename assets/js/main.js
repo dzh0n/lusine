75923a028aca3c21b3540e67f43b505a2774b9a8
Evgeniy Plotnikov <plotnikov.evgeniy@gmail.com>
2022-09-15 13:55:18 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,4 +74,50 @@ $(document).ready(function () {
         $('.actions-slider .pagination-digits_total').text(actionsSliderCurrent[1]);
     });
 
+
+    let certsSlider = new Swiper('.certificates-slider', {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        slidesPerGroup: 3,
+        slidesPerColumnFill: 'row',
+        spaceBetween: 30,
+        navigation: {
+            prevEl: '.cert__prev',
+            nextEl: '.cert__next',
+        },
+        breakpoints: {
+            992: {
+                slidesPerView: 3,
+                slidesPerColumn: 2,
+
+            },
+            576: {
+                slidesPerView: 2,
+                slidesPerColumn: 2,
+                slidesPerGroup: 3,
+            },
+            320: {
+                slidesPerView: 2,
+                slidesPerColumn: 2,
+                slidesPerGroup: 3,
+            },
+        },
+        on: {
+            afterInit(swiper) {
+                //console.log($('.reviews-slider .swiper-slide-active').attr('aria-label'));
+                setTimeout(function () {
+                    let certsSliderCurrent = $('.certificates-slider .swiper-slide-active').attr('aria-label').split(' / ');
+                    $('.certificates-slider .pagination-digits_current').text(certsSliderCurrent[0]);
+                    $('.certificates-slider .pagination-digits_total').text(certsSliderCurrent[1]);
+                }, 0);
+            }
+        }
+    });
+
+    reviewsSlider.on('slideChange', function () {
+        let reviewsSliderCurrent = reviewsSlider.slides.eq(reviewsSlider.activeIndex)[0].ariaLabel.split(' / ');
+        $('.reviews-slider .pagination-digits_current').text(reviewsSliderCurrent[0]);
+        $('.reviews-slider .pagination-digits_total').text(reviewsSliderCurrent[1]);
+    });
+
 });
